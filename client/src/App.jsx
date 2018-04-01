f4b8622904cc01cb3d0a343a8ec60e02712930d3
Will Germany <germany.william@gmail.com>
2018-04-01 17:20:52 -0400

    Added bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Header 1</h1>
-        <h2>Header 2</h2>
-        <h3>Header 3</h3>
-        <h4>Header 4</h4>
-        <h5>Header 5</h5>
+        <h1 className="display-1">Header 1</h1>
+        <h2 className="display-2">Header 2</h2>
+        <h3 className="display-3">Header 3</h3>
+        <h4 className="display-4">Header 4</h4>
+        <button className="btn btn-outline btn-primary">Submit</button>
       </div>
     );
   }
